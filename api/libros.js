6ea851b88ca67b81442b6988a3eeb07d1a6eed27
ruben-arm14 <rubenqc_1405@outlook.com>
2023-11-26 07:24:36 -0500

    comentarios

--- api/libros.js
+++ api/libros.js
@@ -3,24 +3,34 @@ const ruta = express.Router();
 const db = require('../db/models/index.js');
 const { Op } = require("sequelize");
 
+// Ruta para realizar búsquedas avanzadas de libros con filtros y paginación
 ruta.get('/busqueda', async (req, res) => {
+    // Extraer parámetros de la consulta
     const { keyword = '', type = '', filters = [], page = '' } = req.query;
     const pageSize = 5;
     const start = (page - 1) * pageSize;
     const end = start + pageSize;
 
+    // Realizar consulta a la base de datos con Sequelize
     const libros = await db.libro.findAll({
         order: [['id', 'ASC']],
         where: {
+            // Condición OR: al menos una de las condiciones debe ser verdadera
             [Op.or]: {
+                // Condición para el campo 'titulo'
                 titulo: { [Op.iLike]: filters.includes('titulo') ? `%${keyword}%` : 'título_default' },
+                // Condición para el campo 'autor'
                 autor: { [Op.iLike]: filters.includes('autor') ? `%${keyword}%` : 'autor_default' },
+                // Condición para el campo 'topicos'
                 topicos: { [Op.iLike]: filters.includes('genero') ? `%${keyword}%` : 'género_default' },
+                // Condición para el campo 'isbn'
                 isbn: { [Op.iLike]: filters.includes('isbn') ? `%${keyword}%` : 'isbn_default' },
             },
+            // Condición para el campo 'tipo'
             tipo: { [Op.iLike]: `%${type}%` }
         },
         include: {
+            // Incluir información de reservas
             model: db.reserva,
             as: 'reservado',
             attributes: ['id', 'fecha_final'],
@@ -28,12 +38,14 @@ ruta.get('/busqueda', async (req, res) => {
         }
     });
 
+    // Procesar los resultados y agregar información adicional
     const currentDate = new Date();
     const response = libros.map(libro => ({
         ...libro.get({ plain: true }),
         disponible: libro.reservado.some(reserva => new Date(reserva.fecha_final) > currentDate)
     })).slice(start, end);
 
+    // Enviar respuesta JSON con la información paginada
     return res.status(200).json({
         page,
         totalPages: Math.ceil(response.length / pageSize),
@@ -43,8 +55,11 @@ ruta.get('/busqueda', async (req, res) => {
     });
 });
 
+// Ruta para obtener información detallada de un libro
 ruta.get('/leer', async (req, res) => {
+    // Extraer el ID del libro de la consulta
     const { id } = req.query;
+    // Realizar consulta a la base de datos con Sequelize
     const libro = await db.libro.findByPk(id, {
         include: {
             model: db.reserva,
@@ -54,47 +69,63 @@ ruta.get('/leer', async (req, res) => {
         }
     });
 
+    // Procesar los resultados y agregar información adicional
     const currentDate = new Date();
     const response = {
         ...libro.get({ plain: true }),
         disponible: libro.reservado.some(reserva => new Date(reserva.fecha_final) > currentDate)
     };
 
+    // Enviar respuesta JSON con la información del libro
     res.status(200).json(response);
 });
 
+// Ruta para agregar un nuevo libro a la base de datos
 ruta.post('/agregar', async (req, res) => {
+    // Crear un nuevo registro en la base de datos con Sequelize
     const response = await db.libro.create(req.body);
+    // Enviar respuesta JSON con la información del nuevo libro
     res.status(200).json(response);
 });
 
+// Ruta para eliminar un libro de la base de datos
 ruta.delete('/eliminar', async (req, res) => {
+    // Extraer el ID del libro de la consulta y realizar la eliminación con Sequelize
     const { id } = req.query;
     const response = await db.libro.destroy({ where: { id } });
 
+    // Enviar respuesta JSON con el resultado de la eliminación
     res.status(200).json(response);
 });
 
+// Ruta para modificar un libro en la base de datos
 ruta.put('/modificar', async (req, res) => {
+    // Extraer el ID del libro de la consulta y realizar la modificación con Sequelize
     const { id } = req.query;
     const libro = await db.libro.findByPk(id);
     await libro.update(req.body);
+    // Enviar respuesta JSON con un mensaje de éxito
     res.json({ mensaje: 'Libro modificado correctamente' });
 });
 
+// Ruta para obtener los libros más pedidos con paginación
 ruta.get('/MasPedidos', async (req, res) => {
+    // Extraer el número de página de la consulta y configurar la paginación
     const { page } = req.query;
     const pageSize = 2;
     const start = (page - 1) * pageSize;
     const end = start + pageSize;
 
+    // Realizar consulta a la base de datos con Sequelize
     const librosPopulares = await db.libro.findAll({
         where: { contador: { [Op.gt]: 0 } },
         order: [['contador', 'DESC']],
     });
 
+    // Obtener los libros populares según la paginación
     const response = librosPopulares.slice(start, end);
 
+    // Enviar respuesta JSON con la información paginada de los libros populares
     res.status(200).json({
         page,
         totalPages: Math.ceil(response.length / pageSize),
@@ -104,6 +135,7 @@ ruta.get('/MasPedidos', async (req, res) => {
     });
 });
 
+// Función auxiliar para obtener la fecha actual en el formato deseado
 function getCurrentDate() {
     const today = new Date();
     const year = today.getFullYear();
@@ -112,4 +144,5 @@ function getCurrentDate() {
     return `${year}-${month}-${day}`;
 }
 
+// Exportar la ruta para su uso en otras partes de la aplicación
 module.exports = ruta;
--- api/test.js
+++ api/test.js
@@ -1,39 +1,46 @@
+// Importar el framework Express y el objeto Router
 const express = require('express');
 const ruta = express.Router();
+
+// Importar los modelos de la base de datos desde '../db/models/index.js'
 const db = require('../db/models/index.js');
 
+// Rutas para obtener datos de personas, libros y reservas
 ruta.get('/personas', async (req, res) => res.json(await db.persona.findAll()));
-
 ruta.get('/libros', async (req, res) => res.json(await db.libro.findAll()));
-
 ruta.get('/reservas', async (req, res) => res.json(await db.reserva.findAll()));
 
+// Función para obtener una persona con sus relaciones
 const getPersonaWithRelationship = async (id, includeModel) => {
     let entity = await db.persona.findByPk(id, {
         include: [{
             model: includeModel,
-            through: 'reservas',
+            through: 'reservas', // Relación a través de la tabla 'reservas'
             as: 'reservacion',
-            include: []
+            include: [] // No se incluyen más relaciones en este nivel
         }]
     });
 
     if (entity) {
+        // Imprimir información de las reservaciones de la persona
         entity.reservacion.forEach(p => console.log(p.id, p.titulo || p.nombres, p.fecha_inicio, " - ", p.fecha_final));
     }
     return entity;
 };
 
+// Rutas para obtener relaciones entre persona y libro
 ruta.get('/persona_a_libro', async (req, res) => res.status(200).json(await getPersonaWithRelationship(3, db.libro)));
-
 ruta.get('/libro_a_persona', async (req, res) => res.status(200).json(await getPersonaWithRelationship(2, db.persona)));
 
+// Rutas para obtener relaciones entre persona y reserva
 ruta.get('/persona_a_reserva', async (req, res) => res.status(200).json(await getPersonaWithRelationship(2, 'reservante')));
 
+// Ruta para obtener información de una reserva con la persona que la reservó
 ruta.get('/reserva_a_persona', async (req, res) => {
     let reserva = await db.reserva.findByPk(2, {
-        include: ['reservante']
+        include: ['reservante'] // Incluir información del reservante en la respuesta
     });
+
     if (reserva) {
         let p = reserva.reservante;
         console.log(p.id, " - ", p.nombres);
@@ -41,21 +48,24 @@ ruta.get('/reserva_a_persona', async (req, res) => {
     res.status(200).json(reserva);
 });
 
+// Función para obtener una entidad con sus relaciones
 const getEntityWithRelationship = async (id, includeModel) => {
     let entity = await includeModel.findByPk(id, {
-        include: ['reservado']
+        include: ['reservado'] // Incluir información de lo reservado en la entidad
     });
 
     if (entity) {
+        // Imprimir información de las reservas relacionadas con la entidad
         entity.reservado.forEach(p => console.log(p.fecha_inicio, " - ", p.fecha_final));
     }
     return entity;
 };
 
+// Rutas para obtener relaciones entre libro y reserva
 ruta.get('/libro_a_reserva', async (req, res) => res.status(200).json(await getEntityWithRelationship(2, db.libro)));
-
 ruta.get('/reserva_a_libro', async (req, res) => res.status(200).json(await getEntityWithRelationship(2, db.reserva)));
 
+// Ruta para insertar un nuevo libro en la base de datos
 ruta.post('/grabar', async (req, res) => res.status(200).json(await db.libro.create({
     titulo: "Maravillas",
     autor: "grupo5",
@@ -68,4 +78,5 @@ ruta.post('/grabar', async (req, res) => res.status(200).json(await db.libro.cre
     contador: 0
 })));
 
+// Exportar la ruta para su uso en otras partes de la aplicación
 module.exports = ruta;
